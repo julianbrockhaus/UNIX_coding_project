6108124ca042b6fc020554913bbea1bc4d8afa77
Julian <julian.brockhaus@fdmgroup.com>
2014-09-25 11:54:52 +0100

    Exception handling added

--- copy_fr.sh
+++ copy_fr.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
-
-#variable declaration
+#Boolean if find and replace is performed
 fandr=false
-path_source_dir=$(readlink -e $1)
-path_target_dir=$(readlink -e $2)
-source_dir_files=$(ls $1)
 
 #copy files from source into target directory
 function copy(){
@@ -23,13 +19,20 @@ then
 fi
 
 #check if source and target directory exist and if they are read- or writeable
-if [ ! -r $1 ]
+if [ $# -eq 0 -o $# -eq 1 -o $# -eq 3 ]
+then
+	echo "Missing argument"
+	echo "Use: 'source_directory target_directory' or 'source_directory target_directory search_pattern substitution_pattern'"
+elif [ ! -d $1 -o ! -r $1 ]
 then
 	echo "Source directory does not exist or is not readable"
 elif [ ! -w $2 ]
 then
 	echo "Target directory does not exist or is not writeable"
 else
+	path_source_dir=$(readlink -e $1)
+	path_target_dir=$(readlink -e $2)
+	source_dir_files=$(ls $1)
 	#copy all files except directories from source into target directory
 	for j in $source_dir_files
 	do
@@ -43,6 +46,7 @@ else
 
 		if [ $fandr = true ]
 		then
+			echo "Find and replace in $path_target_dir/$j"
 			find_replace $j $3 $4
 		fi
 	done
